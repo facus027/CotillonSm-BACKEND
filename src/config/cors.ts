654a0721cf8659146f36d0389528a4ebf327b351
facus027 <facussguelis@gmail.com>
2025-11-29 20:47:41 -0300

    update cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,18 @@
 import { CorsOptions } from 'cors'
 
+const whitelist = [
+  'https://cotillonsanmartin.com.ar',
+  'https://www.cotillonsanmartin.com.ar',
+]
+
 export const corsConfig: CorsOptions = {
-    origin: function (origin, callback) {
-        const whitelist = [process.env.URL_FRONTEND]
-        
-        if (!origin || whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+  origin(origin, callback) {
+    // origin undefined = peticiones del mismo servidor (curl, SSR, etc.)
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
     }
+  },
+  credentials: true,
 }
